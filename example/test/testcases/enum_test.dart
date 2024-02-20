33cfe4a0e5e5d75812cd9655cf56ff1c7de451fc
fummicc1 <fumiya.tennis1@gmail.com>
2024-02-20 14:34:51 +0900

    Start supporting enum render

---
+++ example/test/testcases/enum_test.dart
@@ -0,0 +1,15 @@
+import 'package:stub_kit/annotations.dart';
+import 'package:test/test.dart';
+
+part 'enum_test.g.dart';
+
+@Stub()
+enum Color { red, green, blue }
+
+void main() {
+  group('stub simple enum', () {
+    test('succeed to make a stub', () {
+      expect(ColorStub.stub(), Color.red);
+    });
+  });
+}
---
+++ example/test/testcases/enum_test.g.dart
@@ -0,0 +1,11 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'enum_test.dart';
+
+// **************************************************************************
+// StubGenerator
+// **************************************************************************
+
+extension ColorStub on Color {
+  static Color stub() => Color.red;
+}
