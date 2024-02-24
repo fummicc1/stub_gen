2ad86dea50c93527938ee9400d077c5e730446d8
Fumiya Tanaka <fumiya.tennis1@gmail.com>
2024-02-24 23:18:37 +0900

    Fix to ignore private constructor if class is freezed. (#8)

---
+++ example/test/testcases/freezed_with_private_constructor_test.dart
@@ -0,0 +1,26 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:stub_kit/annotations.dart';
+import 'package:stub_kit/stubbables.dart';
+import 'package:test/test.dart';
+
+part 'freezed_with_private_constructor_test.freezed.dart';
+part 'freezed_with_private_constructor_test.g.dart';
+
+@Stub(defaultValues: {StubbableTypes.int: 100})
+@freezed
+class FreezedWithPrivateConstructor with _$FreezedWithPrivateConstructor {
+  factory FreezedWithPrivateConstructor({
+    @Default(100) int value,
+  }) = _FreezedWithPrivateConstructor;
+
+  const FreezedWithPrivateConstructor._();
+}
+
+void main() {
+  group('FreezedWithPrivateConstructor', () {
+    test('succeed to make a stub', () {
+      final stub = FreezedWithPrivateConstructorStub.stub();
+      expect(stub.value, equals(100));
+    });
+  });
+}
