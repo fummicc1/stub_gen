a0b698bc1d55ae1a0583629a933c1116d5e31d5c
fummicc1 <fumiya.tennis1@gmail.com>
2024-02-24 22:56:11 +0900

    Fix for private constructor

---
+++ example/test/testcases/private_constructor_test.g.dart
@@ -0,0 +1,12 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'private_constructor_test.dart';
+
+// **************************************************************************
+// StubGenerator
+// **************************************************************************
+
+extension ObjectWithPrivateConstructorStub on ObjectWithPrivateConstructor {
+  static ObjectWithPrivateConstructor stub({int value = 1}) =>
+      ObjectWithPrivateConstructor._(value ?? 1);
+}
--- lib/src/parsers/constructor_parser.dart
+++ lib/src/parsers/constructor_parser.dart
@@ -13,17 +13,20 @@ class ConstructorParser with Parser {
   String parse({required DefaultValues defaultValues}) {
     final className = classElement.name;
     final constructorName = constructorElement.name;
-    if (constructorName.startsWith("_")) {
-      return "";
-    }
+    final isPrivate = constructorName.startsWith("_");
     final parameters = <ParameterParser>[];
     for (final parameter in constructorElement.parameters) {
       parameters.add(ParameterParser(parameter));
     }
+    final stubFactory =
+        constructorName.isEmpty ? className : "$className.$constructorName";
+    final stubConstructorName = isPrivate || constructorName.isEmpty
+        ? "stub"
+        : "${constructorName}Stub";
     return """
-  static $className ${constructorName.isEmpty ? "stub" : "${constructorName}Stub"}({
+  static $className $stubConstructorName({
     ${parameters.map((e) => e.parseForArgument(defaultValues: defaultValues)).where((element) => element.isNotEmpty).join(',\n')}
-  }) => $className${constructorName.isEmpty ? "" : ".$constructorName"}(
+  }) => $stubFactory(
     ${parameters.map((e) => e.parse(defaultValues: defaultValues)).join(',\n')}
   );
 """;
