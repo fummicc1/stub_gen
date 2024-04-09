0422b8ff4f127b534e87456c5d5debf931383227
fummicc1 <fumiya.tennis1@gmail.com>
2024-04-09 10:45:16 +0900

    fix: fix wrong syntax

--- example/test/testcases/named_nullable_parameter_test.dart
+++ example/test/testcases/named_nullable_parameter_test.dart
@@ -20,7 +20,7 @@ class MainClass {
 void main() {
   group('NamedNullableParameterClass', () {
     test('succeed to make a stub', () {
-      final stub = MainClass.stub(customClass: null);
+      final stub = MainClassStub.stub();
       expect(stub.customClass?.name, equals('stub'));
     });
   });
